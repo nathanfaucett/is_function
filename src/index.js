ced5f774a5e7956f117e48bcac77a9ad89f127db
Nathan Faucett <nathanfaucett@gmail.com>
2015-04-30 12:08:31 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-var objectFunction = "[object Function]",
-    toString = Object.prototype.toString,
+var objectToString = Object.prototype.toString,
     isFunction;
 
 
 if (typeof(/./) === "function" || (typeof(Uint8Array) !== "undefined" && typeof(Uint8Array) !== "function")) {
     isFunction = function isFunction(obj) {
-        return toString.call(obj) === objectFunction;
+        return objectToString.call(obj) === "[object Function]";
     };
 } else {
     isFunction = function isFunction(obj) {
