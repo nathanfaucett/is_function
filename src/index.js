d5c56df3c67f8e60a8983ec3ce847407817e7d48
Nathan Faucett <nathanfaucett@gmail.com>
2015-06-14 12:48:41 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-var objectToString = Object.prototype.toString,
-    isFunction;
+var Object_toString = Object.prototype.toString,
+    reFunction, isFunction;
 
 
-if (typeof(/./) === "function" || (typeof(Uint8Array) !== "undefined" && typeof(Uint8Array) !== "function")) {
-    isFunction = function isFunction(obj) {
-        return objectToString.call(obj) === "[object Function]";
+if (Object_toString.call(function() {}) === "[object Object]") {
+    reFunction = /\bfunction\b/;
+    isFunction = function isFunction(value) {
+        return reFunction.test(Object_toString.call(value));
+    };
+} else if (typeof(/./) === "function" || (typeof(Uint8Array) !== "undefined" && typeof(Uint8Array) !== "function")) {
+    isFunction = function isFunction(value) {
+        return Object_toString.call(value) === "[object Function]";
     };
 } else {
-    isFunction = function isFunction(obj) {
-        return typeof(obj) === "function" || false;
+    isFunction = function isFunction(value) {
+        return typeof(value) === "function" || false;
     };
 }
 
